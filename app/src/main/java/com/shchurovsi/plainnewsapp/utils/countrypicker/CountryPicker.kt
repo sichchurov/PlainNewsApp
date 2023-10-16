fc433118de5529049fb0823a3c8b30cd9cd43f64
Sergei Shchurov <si.shchur@gmail.com>
2023-10-16 16:23:40 +0300

    add utils CountryPicker class

--- app/src/main/java/com/shchurovsi/plainnewsapp/utils/countrypicker/CountryPicker.kt
+++ app/src/main/java/com/shchurovsi/plainnewsapp/utils/countrypicker/CountryPicker.kt
@@ -1,4 +1,73 @@
 package com.shchurovsi.plainnewsapp.utils.countrypicker
 
-class CountryPicker {
+import android.content.Context
+import android.util.Log
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.shchurovsi.plainnewsapp.R
+import com.shchurovsi.plainnewsapp.domain.entities.Country
+import com.shchurovsi.plainnewsapp.utils.countrypicker.adapter.CountryPickerAdapter
+import okio.IOException
+
+
+class CountryPicker(
+    val context: Context
+) {
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
+    private val allCountries = getAllCountries()
+
+    private val adapter by lazy {
+        CountryPickerAdapter(context, allCountries)
+    }
+
+    fun attach(bottomSheet: ConstraintLayout): CountryPicker {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        setUpRecyclerView(bottomSheet)
+        return this
+    }
+
+    fun show(): CountryPicker {
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        return this
+    }
+
+    fun dismiss(): CountryPicker {
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        return this
+    }
+
+    private fun getAllCountries(): List<Country> {
+        val countries = mutableListOf<Country>()
+
+        try {
+            val inputStream = context.assets.open("countries.json")
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+
+            val json = String(buffer, Charsets.UTF_8)
+            val gson = Gson()
+            val listType = object : TypeToken<List<Country?>?>() {}.type
+            val listCountry: List<Country> = gson.fromJson(json, listType)
+            countries.addAll(listCountry)
+        } catch (e: IOException) {
+            Log.d(TAG, "Exception: ${e.message}")
+        }
+        return countries.toList()
+    }
+
+    private fun setUpRecyclerView(bottomSheet: ConstraintLayout) {
+        val recyclerView = bottomSheet.findViewById<RecyclerView>(R.id.country_picker_recycler)
+        recyclerView.adapter = adapter
+    }
+
+    companion object {
+        private const val TAG = "CountryPicker"
+    }
 }
--- app/src/main/java/com/shchurovsi/plainnewsapp/utils/countrypicker/adapter/CountryPickerAdapter.kt
+++ app/src/main/java/com/shchurovsi/plainnewsapp/utils/countrypicker/adapter/CountryPickerAdapter.kt
@@ -1,14 +1,25 @@
 package com.shchurovsi.plainnewsapp.utils.countrypicker.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.emoji.bundled.BundledEmojiCompatConfig
 import androidx.emoji.text.EmojiCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.shchurovsi.plainnewsapp.databinding.CountryItemLayoutBinding
 import com.shchurovsi.plainnewsapp.domain.entities.Country
 
-class CountryPickerAdapter(private val countryList: List<Country>) :
+class CountryPickerAdapter(
+    context: Context,
+    private val countryList: List<Country>) :
     RecyclerView.Adapter<CountryPickerViewHolder>() {
+
+    // Initialize EmojiCompat with bundled font support (no need for a FontRequest)
+
+    init {
+        val config = BundledEmojiCompatConfig(context)
+        EmojiCompat.init(config)
+    }
 
     private var onCountrySelectedListener: ((Country) -> Unit)? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryPickerViewHolder {
