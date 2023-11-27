6664c976794884b7bced55529af77c24f9bb6cb8
Sergei Shchurov <si.shchur@gmail.com>
2023-11-27 21:23:27 +0300

    add uiautomator dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,7 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
 
-    implementation("androidx.activity:activity-ktx:1.8.0")
+    implementation("androidx.activity:activity-ktx:1.8.1")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
@@ -95,4 +95,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0-alpha05")
 }
